236da2538a917d2a7d37c5dd45770800a499e9ba
mel-m <ms.melissa.r.m@gmail.com>
2015-03-23 14:50:10 -0400

    Decreased the scope of the variables for the sets

--- LocalSearch/src/DataSets.java
+++ LocalSearch/src/DataSets.java
@@ -15,18 +15,16 @@ import java.util.Hashtable;
  */
 public class DataSets {
 
-	public static Hashtable<String[], Integer> weights = new Hashtable<String[], Integer>();
-	public static ArrayList<String> setOfIngredients = new ArrayList<String>();
-	public static ArrayList<String[]> setOfRecipies= new ArrayList<String[]>();
+	private static Hashtable<String[], Integer> weights = new Hashtable<String[], Integer>();
+	private static ArrayList<String> setOfIngredients = new ArrayList<String>();
+	private static ArrayList<String[]> setOfRecipies= new ArrayList<String[]>();
 
-	
-	
-	
+
 	/**
 	 * creates the all data sets: weights, setOfIngredients, and setOfRecipies
 	 */
 	public static void makeData(){
-		
+
 		setOfIngredients.add("Flour"); 				//a
 		setOfIngredients.add("Milk");				//b
 		setOfIngredients.add("Chilli Peppers");		//c
@@ -35,7 +33,7 @@ public class DataSets {
 		setOfIngredients.add("Butter");				//f
 		setOfIngredients.add("Corn Starch");		//g
 		setOfIngredients.add("Apples");				//h
-		
+
 		//used to make recipe code cleaner
 		String a=setOfIngredients.get(0);
 		String b=setOfIngredients.get(1);
@@ -45,8 +43,8 @@ public class DataSets {
 		String f=setOfIngredients.get(5);
 		String g=setOfIngredients.get(6);
 		String h=setOfIngredients.get(7);
-		
-		
+
+
 		String[] recipie1= {a,b,d};
 		String[] recipie2= {a,g,h,b,e};
 		String[] recipie3= {b,c,d,e};
@@ -54,7 +52,7 @@ public class DataSets {
 		String[] recipie5= {h,g,f,e,a};
 		String[] recipie6= {a,c,e,g};
 		String[] recipie7= {h};
-		
+
 		setOfRecipies.add(recipie1);
 		setOfRecipies.add(recipie2);
 		setOfRecipies.add(recipie3);
@@ -62,7 +60,7 @@ public class DataSets {
 		setOfRecipies.add(recipie5);
 		setOfRecipies.add(recipie6);
 		setOfRecipies.add(recipie7);
-		
+
 		weights.put(recipie1, 4);
 		weights.put(recipie2, 8);
 		weights.put(recipie3, 2);
@@ -70,9 +68,9 @@ public class DataSets {
 		weights.put(recipie5, 7);
 		weights.put(recipie6, 3);
 		weights.put(recipie7, 5);
-			
+
 	}
-	
+
 	/**
 	 * @return Hashtable<String[], Integer> weights
 	 */
@@ -100,7 +98,7 @@ public class DataSets {
 	}
 
 
-	
+
 
 
 }
