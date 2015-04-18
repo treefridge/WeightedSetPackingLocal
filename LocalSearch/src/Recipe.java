395deee408f6fff8cd811a5952831bc502df8d2a
embarg0 <emilienne.pugin@gmail.com>
2015-04-18 19:12:43 -0400

    Implemented, not yet tested

--- LocalSearch/src/Recipe.java
+++ LocalSearch/src/Recipe.java
@@ -1,11 +1,15 @@
 
 public class Recipe {
+	private static int serialNum = 0;
 	private Integer[] ingredients;
 	private int weight;
+	private int recipeNumber;
 	
+
 	public Recipe(Integer[] ingredients, int weight){
 		this.ingredients = ingredients;
 		this.weight = weight;
+		this.recipeNumber = serialNum++;
 	}
 	
 	public Integer[] getIngredients() {
@@ -19,5 +23,15 @@ public class Recipe {
 	}
 	public void setWeight(int weight) {
 		this.weight = weight;
+	}
+	public String toString(){
+		return " R# = " + recipeNumber;
+	}
+	public int getRecipeNumber() {
+		return recipeNumber;
+	}
+
+	public void setRecipeNumber(int number) {
+		this.recipeNumber = number;
 	}
 }
