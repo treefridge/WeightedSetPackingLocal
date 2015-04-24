91a0b85d8198e739dc2d884f7aff67da27f57b5e
maggieplachkov <mplac076@uottawa.ca>
2015-04-24 16:29:32 -0400

    Data set from file

--- LocalSearch/src/DataSet.java
+++ LocalSearch/src/DataSet.java
@@ -1,4 +1,6 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
@@ -44,7 +46,7 @@ public class DataSet {
 		}
 		recipes = new ArrayList<Recipe>();
 
-		Integer[] recipe1= {1,2,3};			//conflicts with 3,7
+/*		Integer[] recipe1= {1,2,3};			//conflicts with 3,7
 		Integer[] recipe2= {4,5,6};			//conflicts with 4,5
 		Integer[] recipe3= {7,8,9,1};		//conflicts with 1,7
 		Integer[] recipe4= {10,11,12,6};	//conflicts with 2,5
@@ -59,6 +61,45 @@ public class DataSet {
 		recipes.add(new Recipe(recipe5, 1));		
 		recipes.add(new Recipe(recipe6, 1));		
 		recipes.add(new Recipe(recipe7, 125));	//necessarily not included in the initial non-greedy solution, but should be in sol iff works out... :/
+		*/
+		
+		int ingredientCount = 0;
+		int recipeCount = 0;
+		//read from input file
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("recipeset.txt"));
+
+	        String line = br.readLine();
+	        //first line
+	        if(line!=null){
+	        	String[] parts = line.split(" ");
+	        	ingredientCount = Integer.parseInt(parts[2]);
+	        	recipeCount = Integer.parseInt(parts[3]);
+	        	line = br.readLine();
+	        }
+
+	        //remaining lines
+	        while (line != null) {
+	        	String[] parts = line.split(" ");
+	        	
+	        	int weight = Integer.parseInt(parts[1]);
+	        	int size = parts.length - 2;
+	        	
+	        	Integer [] recipe = new Integer[size];
+	        	for(int i = 0; i<size; i++){
+	        		recipe[i] = new Integer(parts[i+2]);
+	        	}
+	        	recipes.add(new Recipe(recipe, weight));
+	            line = br.readLine();
+	        }
+
+	        br.close();
+	    } catch(Exception e) {
+	    	System.err.println(e.getMessage());
+	    }
+		
+		//testing purposes
+		System.out.println("IngredientCount= " + ingredientCount + " RecipeCount= " + recipeCount + " ActualRecipeCount= " + recipes.size());
 		
 		return recipes;
 	}
